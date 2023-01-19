40e4130577fad5ce20f6f24f452333b6a4bb3fc4
KangHo-dev <tiger452003@gmail.com>
2023-01-20 00:04:55 +0900

    회원 등록2

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -8,6 +8,9 @@ import javax.validation.constraints.NotEmpty;
 @Getter @Setter
 public class MemberForm {
 
+    /**
+     * 사용자 화면에서 넘어오는 validation 이랑 실제 도메인이 원하는 validation이 다를 수 있다.
+     */
     @NotEmpty(message = "회원 이름은 필수 입니다.")
     private String name;
 
